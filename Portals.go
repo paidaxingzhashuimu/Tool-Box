15f60d006f6bf4d1ca11fcd53047e4a6608f6edb
今晚要吃三碗饭 <103090032+paidaxingzhashuimu@users.noreply.github.com>
2023-07-21 00:27:20 +0800

    Add files via upload

--- Portals.go
+++ Portals.go
@@ -4,24 +4,26 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
-	"tool.com/Gui/Other_go"
+	"tool.com/Box/Other"
 )
 
 func main() {
-	Stute := Other_go.Init_Config()
+	Stute := Other.Init_Config()
 	if Stute == 400 {
 		return
 	}
-	Other_go.My_App = app.New()
+	Other.My_App = app.New()
 	Logo, _ := fyne.LoadResourceFromPath("四次元口袋.ico")
-	Other_go.My_App.SetIcon(Logo)
-	myWindow := Other_go.My_App.NewWindow("四次元口袋  by 今晚要吃三碗饭")
-	myWindow.Resize(fyne.NewSize(1000, 600))
-	myWindow.SetContent(container.NewHBox(
-		container.NewGridWrap(fyne.NewSize(570, 580), Other_go.Left_AppTabs()),
-		container.NewGridWrap(fyne.NewSize(410, 580), Other_go.Right_AppTabs()),
+	Other.My_App.SetIcon(Logo)
+	Other.MyWindow = Other.My_App.NewWindow("四次元口袋  by 今晚要吃三碗饭")
+	Other.MyWindow.Resize(fyne.NewSize(1000, 600))
+	Other.MyWindow.SetContent(container.NewHBox(
+		container.NewGridWrap(fyne.NewSize(580, 580), Other.Left_AppTabs()),
+		container.NewGridWrap(fyne.NewSize(410, 580), Other.Right_AppTabs()),
 	),
 	)
-	myWindow.Show()
-	Other_go.My_App.Run()
+	Other.MyWindow.Show()
+	Other.My_App.Run()
 }
+
+//			Control_Log.Text += ">>添加工具“" + Tmp.Tool_Name + "（id:" + strconv.Itoa(Tmp.Tool_Id) + "）”成功\n\n"
